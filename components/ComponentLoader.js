7bf2899ca6915778b3f3005d7d689d1540cb7803
Flexxbo <flexxbo@gmail.com>
2020-05-21 00:52:11 +0200

    istalled leaflet react react-dom react-leaflet

---
+++ components/ComponentLoader.js
@@ -0,0 +1,21 @@
+/******************************************
+ *  Author : Author
+ *  Created On : Thu May 21 2020
+ *  File : ComponentLoader.js
+ *******************************************/
+
+// This is necessary to load my leaflet-compoment in the browser through componentDidMount
+//referring to: https://stackoverflow.com/questions/57704196/leaflet-with-next-js
+
+/*===Put imports here===*/
+
+/*===Start code here===*/
+
+/* ===Better Comments=== */
+// "* Usage - Comment everything from the beginning, start a task by adding a comment, then work on task
+// "! Alert - Whenever you need an alert
+// "? Review - Look at this again
+// "TODO: "This ..."  is still to do
+// "//commented out
+// "+ Highlight: This needs to be highlited for some reason
+// "@param Explain Parameters
