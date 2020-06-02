84959f051a810a8784323d2a629031c23b4e5967
Flexxbo <flexxbo@gmail.com>
2020-06-02 02:39:41 +0200

    fetching contentful data to tabs

--- components/MapTabs.js
+++ components/MapTabs.js
@@ -1,46 +1,77 @@
+/* 
+! Mit Maptabs weitermachen
+*/
+
 /******************************************
  *  Author : Flexxbo
  *  Created On : Sun May 31 2020
  *  File : Maptabs.js
  *******************************************/
 
+//! Where do I get data from?
+
 /*===Put imports here===*/
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Tab, Tabs, TabList, TabPanel /*resetIdCounter*/ } from "react-tabs";
+import Cliententry from "./Contentfulcomplete";
 
 /*===Start code here===*/
 export default function MapTabs() {
   /*resetIdCounter();
 ReactDOMServer.renderToString(...);*/
 
+  let [data, setData] = useState([]);
+  let [state, setState] = useState({
+    name: "Kuba",
+    tabOne:
+      "Kuba Kuba Kuba Info Info Info Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+  });
+  // ! Need to wait for component to load before using fetched data
 
+  useEffect(() => {
+    Cliententry.getEntries({
+      // * coming from import contentful-component, that connects to API
+      content_type: "destinationContent",
+    })
+      .then((entries) => {
+        console.log("Markers entries in Maptabs:", entries);
+        setData(entries.items);
+        console.log(state);
+        console.log("Maptabs InUseEffect data:", data);
+        console.log("Maptabs InUseEffect data[0]:", data[0]);
+        console.log(
+          "Maptabs InUseEffect data[0].fields.name:",
+          data[0].fields.name
+        );
+        console.log(
+          "Maptabs InUseEffect data[0].name:",
+          JSON.stringify(data[0].name)
+        );
+
+        // * entries is object, items is array, need array to map below
+      })
+      .catch((err) => console.log("An error occured: " + err));
+  }, []);
+  console.log("Maptabs Outside of UseEffect data:", data);
+  // ! Lifecycle Problem not fetching properly
 
   return (
     <div className="MapTabsInner">
+      {/*data[0].fields.name*/}
       <Tabs>
         <TabList>
-          <Tab>Tab 1</Tab>
-          <Tab>Tab 2</Tab>
-          <Tab>Tab 3</Tab>
-          <Tab>Tab 4</Tab>
+          <Tab>{state.name}</Tab>
+          <Tab>Sightseeing</Tab>
+          <Tab>Accomodation</Tab>
+          <Tab>Transport</Tab>
         </TabList>
 
         <TabPanel>
-          <h2>Lorem Ipsum 1</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum.d diam nonumy eirmod tempor invidunt ut labore et dolore
-            magna aliquyam erat, sed Stet clita kasd gubergren, no sea takimata
-            sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
-            consetetur sadipscing elitr, se diam voluptua. At vero eos et
-            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-            no sea takimata sanctus est Lorem ipsum dolor sit amet.
-          </p>
+          <h2>Header tabOne</h2>
+          <p>{state.tabOne}</p>
         </TabPanel>
         <TabPanel>
-          <h2>Lorem Ipsum 2</h2>
+          <h2>Header tabTwo</h2>
           <p>
             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
             nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
@@ -54,7 +85,7 @@ ReactDOMServer.renderToString(...);*/
           </p>
         </TabPanel>
         <TabPanel>
-          <h2>Lorem Ipsum 3</h2>
+          <h2>Header tabThree</h2>
           <p>
             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
             nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
@@ -68,7 +99,7 @@ ReactDOMServer.renderToString(...);*/
           </p>
         </TabPanel>
         <TabPanel>
-          <h2>Lorem Ipsum 4</h2>
+          <h2>Header tabFour</h2>
           <p>
             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
             nonumy eirmod tempor invidu et accusam et justo duo dolores et ea
