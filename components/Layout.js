8390c9c703dad545f2678f2efdadbd12e63583c1
Flexxbo <flexxbo@gmail.com>
2020-06-08 16:03:38 +0200

    fixed typo

--- components/Layout.js
+++ components/Layout.js
@@ -23,7 +23,7 @@ const Layout = (props) => (
         rel="stylesheet"
         href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
         integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
-        crossorigin="anonymous"
+        crossOrigin="anonymous"
       ></link>
       <link
         rel="stylesheet"
